a460f0f421ee5556c44b132f960633ac89f59765
danbinnun1 <danbinnun1@gmail.com>
2021-03-31 18:20:26 +0300

    added exec

--- ex1.c
+++ ex1.c
@@ -1,12 +1,17 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #define COMMAND_MAX_LENGTH 100
 #define IN_QUOTATION 0
 #define IN_SPACE 1
 #define IN_WORD 2
 
-void parseCommand(const char* command, char args[][100]) {
+char** parseCommand(const char* command) {
+    char args[COMMAND_MAX_LENGTH][COMMAND_MAX_LENGTH];
     const char* offset = command;
     uint8_t length = 0;
     uint8_t argNum = 0;
@@ -58,17 +63,32 @@ void parseCommand(const char* command, char args[][100]) {
         }
         offset++;
     }
-    int t=8;
+    char** result = malloc(sizeof(char*) * (argNum + 2));
+    for (uint8_t i = 0; i < argNum+1; i++) {
+        result[i] = malloc(strlen(args[i]) + 1);
+        strcpy(result[i], args[i]);
+    }
+    result[argNum+1] = NULL;
+    return result;
 }
 
 int main() {
+    execlp("l", "l",NULL);
     while (1) {
         printf("$ ");
         fflush(stdout);
         char command[COMMAND_MAX_LENGTH];
-        scanf("%[^\n]s", command);
-        char args[COMMAND_MAX_LENGTH][COMMAND_MAX_LENGTH];
+        scanf("%100s", command);
         // split by white character
-        parseCommand(command, (char**)args);
+        char** args = parseCommand(command);
+        pid_t pid = fork();
+        if (pid == 0) {
+            if (execvp(args[0], args) == -1) {
+                printf("exec failed\n");
+                exit(0);
+            }
+        } else {
+            waitpid(pid, NULL, 0);
+        }
     }
 }
